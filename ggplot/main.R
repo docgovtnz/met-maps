f94c6c94c49711ffbf46e032fc4531d86d2f3b34
Jordi Tablada <jtablada@doc.govt.nz>
2023-05-12 14:29:19 +1200

    Code reformatted

--- ggplot/main.R
+++ ggplot/main.R
@@ -11,12 +11,15 @@ library(sf)
 # Shannon-Wiener Index (`H` column) of NA value
 
 df <- read.csv("../data/tuhua.csv", stringsAsFactors = FALSE)
-df <- df[is.na(df$H)==FALSE, ]
+df <- df[is.na(df$H) == FALSE,]
 
 # 2. Load Marine Reserves GeoJSON from Marine Data Portal
 # https://services1.arcgis.com/3JjYDyG3oajxU6HO/arcgis/rest/services/DOC_Marine_Reserves/FeatureServer/0
 
-tuhua.mr <- readOGR("https://services1.arcgis.com/3JjYDyG3oajxU6HO/arcgis/rest/services/DOC_Marine_Reserves/FeatureServer/0/query?where=Name%20%3D%20'TUHUA%20(MAYOR%20ISLAND)%20MARINE%20RESERVE'&outFields=Name,Shape__Area,Shape__Length&outSR=4326&f=json")
+tuhua.mr <-
+  readOGR(
+    "https://services1.arcgis.com/3JjYDyG3oajxU6HO/arcgis/rest/services/DOC_Marine_Reserves/FeatureServer/0/query?where=Name%20%3D%20'TUHUA%20(MAYOR%20ISLAND)%20MARINE%20RESERVE'&outFields=Name,Shape__Area,Shape__Length&outSR=4326&f=json"
+  )
 
 # 3. Get New Zealand coountry data from geodata package
 # https://github.com/rspatial/geodata
@@ -32,15 +35,40 @@ mr.df <- spTransform(tuhua.mr, crs(nz.gadm))
 # 4. Arrange all data in a ggplot object
 # See https://ggplot2.tidyverse.org/
 
-plt <- ggplot(nz.df) + geom_sf(fill="grey", alpha=0.3) +
-  geom_polygon(data = mr.df, aes(x = long, y = lat, group = group), colour="#00ff00", fill="#00ff00", alpha=0.2) +
-  coord_sf(xlim = c(176.2092899568599, 176.30032714224936), ylim = c(-37.245069509603134, -37.321945636347586)) +
-  geom_point(data=df , aes(x=Long, y=Lat, size=H, colour=factor(Year)), alpha=0.5) +
+plt <- ggplot(nz.df) + geom_sf(fill = "grey", alpha = 0.3) +
+  geom_polygon(
+    data = mr.df,
+    aes(x = long, y = lat, group = group),
+    colour = "#00ff00",
+    fill = "#00ff00",
+    alpha = 0.2
+  ) +
+  coord_sf(
+    xlim = c(176.2092899568599, 176.30032714224936),
+    ylim = c(-37.245069509603134,-37.321945636347586)
+  ) +
+  geom_point(data = df ,
+             aes(
+               x = Long,
+               y = Lat,
+               size = H,
+               colour = factor(Year)
+             ),
+             alpha = 0.5) +
   scale_colour_manual(values = c("#6666FF", "#FFA500")) + theme_bw() + theme(axis.title = element_blank()) +
-  labs(colour="Year", size="Shannon-Wiener Index") +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+  labs(colour = "Year", size = "Shannon-Wiener Index") +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
 
 # 5. Show plot in Plots pane (if using RStudio) and save to disk
 # Can also be exported from RStudio editing resolution and other details
 plt
-ggsave("./tuhua.png",width = 2200, height = 2200, units = "px" ,plot = plt, dpi = 300, type="cairo")
+ggsave(
+  "./tuhua.png",
+  width = 2200,
+  height = 2200,
+  units = "px",
+  plot = plt,
+  dpi = 300,
+  type = "cairo"
+)
--- mapview/main.R
+++ mapview/main.R
@@ -15,40 +15,54 @@ library(rgdal)
 # Shannon-Wiener Index (`H` column) of NA value
 
 df <- read.csv("../data/tuhua.csv", stringsAsFactors = FALSE)
-df <- df[is.na(df$H)==FALSE, ]
+df <- df[is.na(df$H) == FALSE,]
 
 # 2. Load Marine Reserves GeoJSON from Marine Data Portal
 # already filter using the `where` parameter
 # https://services1.arcgis.com/3JjYDyG3oajxU6HO/arcgis/rest/services/DOC_Marine_Reserves/FeatureServer/0
 
-tuhua.mr <- readOGR("https://services1.arcgis.com/3JjYDyG3oajxU6HO/arcgis/rest/services/DOC_Marine_Reserves/FeatureServer/0/query?where=Name%20%3D%20'TUHUA%20(MAYOR%20ISLAND)%20MARINE%20RESERVE'&outFields=Name,Shape__Area,Shape__Length&outSR=4326&f=json")
+tuhua.mr <-
+  readOGR(
+    "https://services1.arcgis.com/3JjYDyG3oajxU6HO/arcgis/rest/services/DOC_Marine_Reserves/FeatureServer/0/query?where=Name%20%3D%20'TUHUA%20(MAYOR%20ISLAND)%20MARINE%20RESERVE'&outFields=Name,Shape__Area,Shape__Length&outSR=4326&f=json"
+  )
 
 # 3. Define palette of minimum 3 colors although we pick as many as unique year values
 
 min.cols <- length(unique(df$Year))
-pal <- brewer.pal(
-  n = max(3, min.cols),
-  name="Set2")[1:min.cols]
+pal <- brewer.pal(n = max(3, min.cols),
+                  name = "Set2")[1:min.cols]
 
 # 4. Generate mapview instance in `m` variable
-# 
-# CSV data loaded into dataframe df is plotted with dot size according to `H` 
+#
+# CSV data loaded into dataframe df is plotted with dot size according to `H`
 # (Shannon-Wiener Index) value and colored by Year using the `pal` colors.
-# 
+#
 # GeoJSON containing Tuhua MR plotted in green
 #
-m <- mapview(df, layer.name = c("Shannon-Wiener Index"),
-             xcol="Long", ycol="Lat", zcol="Year", cex="H", crs=4326, grid=FALSE,
-             col.regions=pal, legend=TRUE) + mapview(tuhua.mr, layer.name = tuhua.mr$Name,
-               color = 'green', alpha.regions = 0.2, col.regions="green")
+m <- mapview(
+  df,
+  layer.name = c("Shannon-Wiener Index"),
+  xcol = "Long",
+  ycol = "Lat",
+  zcol = "Year",
+  cex = "H",
+  crs = 4326,
+  grid = FALSE,
+  col.regions = pal,
+  legend = TRUE
+) + mapview(
+  tuhua.mr,
+  layer.name = tuhua.mr$Name,
+  color = 'green',
+  alpha.regions = 0.2,
+  col.regions = "green"
+)
 
 # show map on Viewer pane (if using Rstudio)
 m
 
 # 5. Save mapshot to html file
-# Open the resulting `index.html` in your local browser or publish it in a Github 
+# Open the resulting `index.html` in your local browser or publish it in a Github
 # repository if you are looking to share it with others.
 # See https://pages.github.com/ for more information on how to publish
 mapshot(m, url = "./index.html")
-
-m
